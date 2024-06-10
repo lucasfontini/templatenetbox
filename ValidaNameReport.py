44506c5810d31d6405a4eb7cf0f906e4202df69a
lucasfontini <lucasfacsul@hotmail.com>
2024-06-10 15:09:15 -0300

    Update ValidaNameReport.py

--- ValidaNameReport.py
+++ ValidaNameReport.py
@@ -4,17 +4,17 @@ from dcim.choices import DeviceStatusChoices
 from dcim.models import Device
 from extras.reports import Report
 
-# A modified John Anderson's NetBox Day 2020 Presentation by adding a check for all sites, not just LAX
-# All credit goes to @lampwins
-
 class DeviceHostnameReport(Report):
-   description = "Verify each device conforms to naming convention Example: ABC.5555.A555.PE05"
+    description = "Verify each device conforms to naming convention Example: ABC.5555.A555.PE05"
 
-   def test_device_naming(self):
-       for device in Device.objects.filter(status=DeviceStatusChoices.STATUS_ACTIVE):
-           # Change the naming standard based on the re.match
-           pattern = r"[A-Za-z0-9]{3,4}\.[0-9]{3,6}\.[A-Z][0-9]{3}.PE[0-9]{1,2}"
-           if re.match(pattern, str(device.name), re.IGNORECASE):
-               self.log_success(device.name)
-           else:
-               self.log_failure(device, "Hostname does not conform to standard!")
+    def test_device_naming(self):
+        # Regex pattern to match the naming convention
+        pattern = r"[A-Za-z0-9]{3,4}\.[0-9]{3,6}\.[A-Z][0-9]{3}\.PE[0-9]{1,2}"
+        
+        for device in Device.objects.filter(status=DeviceStatusChoices.STATUS_ACTIVE):
+            # Check if the device name matches the pattern
+            self.info("Cheking", device.name)
+            if re.match(pattern, device.name):
+                self.log_success(device.name)
+            else:
+                self.log_failure(device, "Hostname does not conform to standard!")
