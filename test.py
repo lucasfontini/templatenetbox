16c619ca061096568f2fd22d29c55eb47bbac139
lucasfontini <lucasfacsul@hotmail.com>
2024-06-16 17:42:33 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,16 @@
-
-import json
 from extras.scripts import Script, StringVar, ObjectVar
 from dcim.choices import DeviceStatusChoices
 from dcim.models import Device, DeviceRole, DeviceType, Site, Interface
 from ipam.models import IPAddress
 from extras.models import Tag, ConfigTemplate, ConfigContext
- 
+
+class ShowDataScript(Script):
+
+    class Meta:
+        name = "Exibir Dados Recebidos"
+        description = "Script para exibir os dados recebidos ao ser executado"
+
     def run(self, data, commit):
-       self.log_info(data)
+        self.log_info(f"Dados recebidos: {json.dumps(data, indent=4)}")
 
-
-        return f"terminou"
+        return "terminou"
