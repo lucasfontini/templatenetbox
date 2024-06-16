d339929dcd696efb7754527d7cb3b736a5e3f587
lucasfontini <lucasfacsul@hotmail.com>
2024-06-16 17:41:20 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
 
+import json
+from extras.scripts import Script, StringVar, ObjectVar
+from dcim.choices import DeviceStatusChoices
+from dcim.models import Device, DeviceRole, DeviceType, Site, Interface
+from ipam.models import IPAddress
+from extras.models import Tag, ConfigTemplate, ConfigContext
+ 
+    def run(self, data, commit):
+       self.log_info(data)
+
+
+        return f"terminou"
