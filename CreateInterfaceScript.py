a8c1d872812226e60ec7c29af8ae8ba2832aac1c
lucasfontini <lucasfacsul@hotmail.com>
2024-08-10 22:58:39 -0300

    Update CreateInterfaceScript.py

--- CreateInterfaceScript.py
+++ CreateInterfaceScript.py
@@ -1,7 +1,8 @@
-from extras.scripts import Script, ChoiceVar, ObjectVar, IPAddressWithMaskVar
+from extras.scripts import Script, ChoiceVar, ObjectVar
 from dcim.models import Device, Interface
-from ipam.models import IPAddress
+from ipam.models import IPAddress, VLAN
 from django.contrib.contenttypes.models import ContentType
+from ipam.models import IPAddress
 
 class CreateInterfaceScript(Script):
     class Meta:
@@ -20,8 +21,9 @@ class CreateInterfaceScript(Script):
         required=True
     )
 
-    ip_manual = IPAddressWithMaskVar(
+    ip_manual = ObjectVar(
         description="Selecione o IP no formato 192.168.2.1/30",
+        model=IPAddress,
         required=False  # Ajuste para não ser obrigatório, já que o IP pode não existir ainda
     )
 
@@ -77,14 +79,13 @@ class CreateInterfaceScript(Script):
 
             # Cadastrar o IP se não estiver cadastrado
             if ip_manual:
-                ip_address = IPAddress.objects.filter(address=ip_manual).first()
-                if ip_address is None:
-                    self.log_failure(f"IP '{ip_manual}' não encontrado.")
-                    return f"Falha ao encontrar o IP '{ip_manual}'."
+                ip_address = IPAddress.objects.filter(id=ip_manual.id).first()
             else:
                 # Cria o IP manualmente
                 ip_address = IPAddress(
-                    address="192.168.2.1/30",  # Ajuste o IP conforme necessário
+                    address="200.100.10.1/30",  # Ajuste o IP conforme necessário
+                    assigned_object=interface_gre,
+                    assigned_object_type=ContentType.objects.get_for_model(interface_gre)
                 )
                 if commit:
                     try:
@@ -92,22 +93,20 @@ class CreateInterfaceScript(Script):
                         self.log_success(f"IP '{ip_address}' criado com sucesso.")
                     except Exception as e:
                         self.log_failure(f"Falha ao criar IP: {str(e)}")
-                        return f"Falha ao criar IP: {str(e)}"
                 else:
                     self.log_info(f"Simulação: IP '{ip_address}' seria criado.")
 
             # Associar o IP à interface GRE
-            if ip_address:
-                if commit:
-                    try:
-                        ip_address.assigned_object = interface_gre
-                        ip_address.assigned_object_type = ContentType.objects.get_for_model(interface_gre)
-                        ip_address.save()
-                        self.log_success(f"IP '{ip_address}' configurado na interface GRE '{interface_gre_name}'.")
-                    except Exception as e:
-                        self.log_failure(f"Falha ao configurar IP na interface GRE: {str(e)}")
-                else:
-                    self.log_info(f"Simulação: IP '{ip_address}' seria configurado na interface GRE '{interface_gre_name}'.")
+            if commit:
+                try:
+                    ip_address.assigned_object = interface_gre
+                    ip_address.assigned_object_type = ContentType.objects.get_for_model(interface_gre)
+                    ip_address.save()
+                    self.log_success(f"IP '{ip_address}' configurado na interface GRE '{interface_gre_name}'.")
+                except Exception as e:
+                    self.log_failure(f"Falha ao configurar IP na interface GRE: {str(e)}")
+            else:
+                self.log_info(f"Simulação: IP '{ip_address}' seria configurado na interface GRE '{interface_gre_name}'.")
 
         else:
             self.log_info(f"A solução escolhida '{solucao}' não requer a criação de interfaces.")
